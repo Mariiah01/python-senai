f288cccf989458fc51b5c54cf38b8cd715a1f197
Mariiah01 <marialuiizaa4512@gmail.com>
2024-12-03 11:39:00 -0300

    Add files via upload

---
+++ Aula 2/aula02.py
@@ -0,0 +1,11 @@
+print('Bom dia a todos presentes')
+
+nome_usuario = 'maria luiza'# tipo -> str = string
+idade_usuario = 16 # tipo -> int = integer
+altura_usuario = 1.57 # tipo -> float= ponto flutuante
+maior_idade = False # tipo -> bool = boolean 
+
+print('seja bem-vindo' , nome_usuario,)
+print('sua idade e ' , idade_usuario, 'anos')
+print('voce tem' ,altura_usuario,"metros." )
+print( 'voce e maior de idade?' , maior_idade ) 
